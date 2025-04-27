badbef9f6d444bbc3017f3d826fab846041c9bd1
Vvendet <froees147@gmail.com>
2025-04-27 16:04:52 -0300

    a

--- Lista 2/Questao B/Q2B1.f90
+++ Lista 2/Questao B/Q2B1.f90
@@ -4,52 +4,34 @@ program QuestaoA
     implicit none
 
     real, dimension(100)::x
-    integer::i,escolha
-    real:: a,b,c
+    integer::i
 
-    write(*,*)"Escreva 1, para newton raphson"
-    write(*,*)"Escreva 2, para bissecao"
-    write(*,*)"escreva 3, para ponto fixo"
-    read(*,*)escolha
-    if (escolha==2) then
-        write(*,*) "Escolha os valores a e b do intervalo, respectivamente:"
-        read(*,*)a
-        read(*,*)b
-    else
-        write(*,*)"Escolha o chute inicial"
-        read(*,*)x(1)
-    end if
-
-    open(unit=777,file='saida.dat',status='unknown')
+    x(1)=-0.1
     do i =2,100
-        if (escolha == 1) then
-            x(i)=n(x(i-1))
-            write(777,*)i,x(i)
 
-        else if (escolha==2) then
-            x(i)=(a+b)/2
-            c = f(x(i))
-
-            if (c>0) then
-                b=x(i)
-
-            else if (c<0) then
-                a=x(i)
-
-            end if
-
-            write(777,*)i, x(i)
+        x(i)=n(x(i-1))
+        if (abs(x(i)-(-4.08773199E-02))<0.00001) then
 
 
-        else if (escolha==3) then
-            x(i)=p(x(i-1))
-            write(777,*)i,x(i)
+            write(*,*)i,x(i)
+            exit
 
         end if
+
     end do
-    close(unit=777)
 
 
+    x(1)=0.96
+    do i =2,100
+        x(i)=n(x(i-1))
+        if (abs(x(i)-(0.962398410))<0.00001) then
+
+            write(*,*)i,x(i)
+            exit
+
+        end if
+
+    end do
 
     contains
 
@@ -58,19 +40,9 @@ program QuestaoA
 
         real::x,n
 
-        n = (230*x*x*x*x+18*x*x*x+9*x*x-221*x-9)-(230*x*x*x*x+18*x*x*x+9*x*x-221*x-9)/(4*230*x*x*x+3*18*x*x+2*9*x-221)
+        n = x-f(x)/flinha(x)
 
         return
-
-    end function
-
-    function p(x)
-        implicit none
-
-        real::p,x
-
-        p = sqrt(sqrt((9+221*x-9*x*x+18*x*x*x)/230))
-
 
     end function
 
@@ -81,5 +53,13 @@ program QuestaoA
         f = 230*x*x*x*x+18*x*x*x+9*x*x-221*x-9
 
         return
+    end function
+
+    function flinha(x)
+        implicit none
+        real::flinha,x
+
+        flinha = 4*230*x*x*x+3*18*x*x+18*x-221
+
     end function
 end program

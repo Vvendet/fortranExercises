1b5c7dd37296025f2e80d964bfc8dc14e560a136
Júlio <66224435+Vvendet@users.noreply.github.com>
2025-05-13 12:45:03 -0300

    Add files via upload

--- non-linear-system.f90
+++ non-linear-system.f90
@@ -9,17 +9,24 @@ program lista4
   allocate(x(n))
   allocate(y(n))
   allocate(z(n))
-  x(1)=0.1d0
-  y(1)=0.1d0
-  z(1)=0.1d0
+  !x(1)=0.1d0
+  !y(1)=0.1d0
+  !z(1)=0.1d0
+
+  x(1)=0.25d0
+  y(1)=0.25d0
 
   do i=2,100
+     if (y(i-1)<0) then
+        x(i)=h12(y(i-1))
+        
+     else
+        x(i)=h11(y(i-1))
+        
+     end if
+     y(i)=h2(x(i-1),y(i-1))
 
-     x(i)=g1(y(i-1),z(i-1))
-     y(i)=g2(x(i),z(i-1))
-     z(i)=g3(x(i-1),y(i))
-
-     write(*,*)i,x(i),y(i),z(i)
+     write(*,*)i,x(i),y(i)
      
   end do
 
@@ -57,6 +64,35 @@ contains
     real(8), parameter :: pi=3.14159274D0
 
     g3=(3.d0-10.d0*pi)/60.d0-1.d0/20.d0*exp(x1*x2)
+
   end function g3
-  
+
+
+
+  function h11(x2)
+    implicit none
+
+    double precision::x2,h11
+
+    h11=x2/sqrt(5.d0)
+
+  end function h11
+
+  function h12(x2)
+    implicit none
+
+    double precision::x2,h12
+
+    h12=-x2/sqrt(5.d0)
+    
+  end function h12
+
+  function h2(x1,x2)
+    implicit none
+    double precision::x1,x2,h2
+
+    h2=(sin(x1)+cos(x2))/4.d0
+    
+
+  end function h2
 end program lista4
